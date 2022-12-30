369b06b3ea668cbe1f4a7c5d30d599641f1ae209
Szymon Boniszewski <szymkop2001@gmail.com>
2022-12-30 13:32:54 +0100

    saving to file

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from lxml import html
 import requests
+import datetime
+
+path = "C://Users//szymk//Desktop//Strzaly.txt"
 
 def list_to_string(s):
     news = ""
@@ -7,7 +10,25 @@ def list_to_string(s):
         news += x
     return news
 
+def find(find_it):
+    file_to_search = open(path)
+    read = file_to_search.read()
+    index = read.find(find_it)
+    if index>-1:
+        return True
+    return False
+
+def save(dane):
+    file_to_save = open(path, 'a+')
+    file_to_save.write(f"{dane}\n\n")
+    print("Zapisano!")
+
 def request():
+    date = datetime.datetime.now()
+    if date.hour<22 and date.hour>9:
+        date_string = f"{date.day}-{date.month}-{date.year} Godzina: 10"
+    else:
+        date_string = f"{date.day}-{date.month}-{date.year} Godzina: 22"
 
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 OPR/93.0.0.0'}
     html_file = requests.get("https://www.x-kom.pl", headers=headers)
@@ -19,9 +40,21 @@ def request():
     ilosc = tree.xpath('//*[@id="hotShot"]/div/a/div/div[2]/p/text()')
     co = tree.xpath('//*[@id="hotShot"]/div/a/div/div[1]/div/span/img/@alt')
     
-    result = (f"Gorący Strzał teraz:\n{list_to_string(co)}\nStara cena: {list_to_string(cenastara)}\n"
+    result = (f"Data: {date_string}\nGorący Strzał teraz:\n{list_to_string(co)}\nStara cena: {list_to_string(cenastara)}\n"
     f"Nowa cena: {list_to_string(cenanowa)}\n{list_to_string(oszczednosc)}\nZostało sztuk: {list_to_string(ilosc)}")
 
     return result
 
-print(request())
+output = request()
+
+date = datetime.datetime.now()
+if date.hour<22 and date.hour>9:
+    date_string = f"{date.day}-{date.month}-{date.year} Godzina: 10"
+    if not find(date_string):
+        save(output)
+else:
+    date_string = f"{date.day}-{date.month}-{date.year} Godzina: 22"
+    if not find(date_string):
+        save(output)
+
+print(output)
